1b991296b774103baafa13eba0cda7bb3930e023
helmimarif <helmimarif11@gmail.com>
2022-11-30 18:02:04 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Ayse_Base
+
+Basic Resources for AyseFramework
+
+### config.cfg
+
+```cfg
+ensure mapmanager
+ensure chat
+ensure spawnmanager
+ensure sessionmanager
+ensure basic-gamemode
+ensure hardcap
+```
+
+## Dependency:
+[Ayse Core](https://github.com/ayse-framework/Ayse_Core)
---
+++ [system]/Ayse_Map/map.lua
@@ -0,0 +1,6 @@
+spawnpoint 'mp_m_freemode_01' { x = -1037.8712, y = -2737.9082, z = 13.7968 }
+spawnpoint 'mp_m_freemode_01' { x = -747.2906, y = -1305.2629, z = 5.0004 }
+spawnpoint 'mp_f_freemode_01' { x = -1037.8712, y = -2737.9082, z = 13.7968 }
+spawnpoint 'mp_f_freemode_01' { x = -747.2906, y = -1305.2629, z = 5.0004 }
+
+--
